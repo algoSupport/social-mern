5c67d6b4f84b8930aea75915d815bb784a8cd990
algoSupport <algoplusnine@outlook.com>
2021-08-25 06:27:34 +0300

    changed class -> className in navbar.js

--- app.js
+++ app.js
@@ -26,13 +26,14 @@ app.use(require('./routes/post'))
 app.use(require('./routes/user'))
 
 
-if(process.env.NODE_ENV=="production"){
+// if(process.env.NODE_ENV=="production"){
+    console.log("production build");
     app.use(express.static('client/build'))
     const path = require('path')
     app.get("*",(req,res)=>{
         res.sendFile(path.resolve(__dirname,'client','build','index.html'))
     })
-}
+// }
 
 app.listen(PORT,()=>{
     console.log("server is running on",PORT)
